130d1f5726d29080a08845c69292268a27de6a21
rmbodegalibre <rmbodegalibre@gmail.com>
2022-02-10 22:17:04 -0500

    My first project of C (Corrected2)

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  * Print message using printf
+ *   * Return: 0
+ *    */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
