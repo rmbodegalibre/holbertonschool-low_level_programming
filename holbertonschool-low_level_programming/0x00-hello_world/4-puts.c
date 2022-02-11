68423e5046274353b6ee85ac5d4414f5928fefbd
rmbodegalibre <rmbodegalibre@gmail.com>
2022-02-10 21:54:16 -0500

    My first project of C (Corrected)

---
+++ holbertonschool-low_level_programming/0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  * Print message using puts
+ *   * End with Return 0
+ *    */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
