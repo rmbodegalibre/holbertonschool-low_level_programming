e0822fe4ab17140c84685937438bfebc54770a55
rmbodegalibre <rmbodegalibre@gmail.com>
2022-02-15 12:17:27 -0500

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef _main_h_
+#define _main_h_
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
@@ -9,9 +9,12 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
-
 /**
  * print_alphabet - Print alphabet one time
  */
 void print_alphabet(void);
+/**
+ * print_alphabet_x10 - Print alphabet ten times followed by new line
+ */
+void print_alphabet_x10(void);
 #endif
