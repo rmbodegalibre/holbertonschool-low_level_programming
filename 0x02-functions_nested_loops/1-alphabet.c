38a000fb39ffa7c9a54be856e8619e9c8d431592
rmbodegalibre <rmbodegalibre@gmail.com>
2022-02-15 06:46:13 -0500

    0. _putchar_03

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,5 @@
 #include "main.h"
 /**
- * print_alphabet - print alphabet, lowercase, followed by a new line.
- * Only use _putchar
- * Return: 0
- */
-void print_alphabet(void);
-
-/**
  * main - check the code
  *
  * Return: Always 0.
@@ -15,19 +8,4 @@ int main(void)
 {
 	print_alphabet();
 	return (0);
-}
-/**
- * print_alphabet - print alphabet, lowercase, followed by a new line.
- * Only use _putchar
- * Return: 0
- */
-void print_alphabet(void)
-{
-	char i;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-	_putchar(i);
-	}
-	_putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #include <unistd.h>
-
+void print_alphabet(void);
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -10,4 +10,18 @@
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+/**
+ * print_alphabet - print alphabet, lowercase, followed by a new line.
+ * Only use _putchar
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
 }
