bcdc58320e3fad604401a2273ea8f942d947d2c6
rmbodegalibre <rmbodegalibre@gmail.com>
2022-02-15 09:57:59 -0500

    0. _putchar_004

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,23 +9,5 @@ void print_alphabet(void);
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
- * print_alphabet - print alphabet,lowercase,followed by new line.
- * Only use _putchar
- * Return: 0
- */
-void print_alphabet(void)
-{
-	char i;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-	_putchar(i);
-	}
-	_putchar('\n');
-}
+int _putchar(char c);
 #endif
