60feaa2b9c0414452972ef64466ec0e0bf457b79
rmbodegalibre <rmbodegalibre@gmail.com>
2022-03-04 11:46:28 -0500

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * main - check code
+ * @argc: Number of arguments
+ * @argv: Array that contain arguments
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
