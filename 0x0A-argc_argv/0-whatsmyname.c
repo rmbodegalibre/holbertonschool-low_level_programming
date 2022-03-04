94b055e885981a5f770dc0290476afe7594b8627
rmbodegalibre <rmbodegalibre@gmail.com>
2022-03-04 12:27:33 -0500

    0-whatsmyname.c and 1-args.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
+/**
  * main - check code
  * @argc: Number of arguments
  * @argv: Array that contain arguments
+ * Description: print number of arguments
+ * Return: Always 0
  */
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s", argv[0]);
-	return (0);
+	return (0); /*Return: Always 0*/
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
+/**
  * main - check code
  * @argc: Number of arguments
  * @argv: Array that contain arguments
+ * Description: Prints filename
+ * Return: Always 0
  */
+
 
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	        printf("%d", argc - 1);
-		        return (0);
+	printf("%d", argc - 1);
+	return (0);
 }
