3f9693ea1d5ccccb3a7c08734e63d94f69d5a3a7
rmbodegalibre <rmbodegalibre@gmail.com>
2022-03-15 00:01:39 -0500

    5. How many legs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,17 @@
 #include "dog.h"
 #include <stdlib.h>
+
 /**
  * free_dog - frees dogs
- * @d: variable to be free
- * Return: Nothing
+ * @d: pointer dog_t type input
+ * Return: Nothing.
  */
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
-
 	free(d->name);
 	free(d->owner);
 	free(d);
-
 	}
 }
