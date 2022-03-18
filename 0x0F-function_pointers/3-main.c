7727ad3e1249e192527fb4085f7fe5dc5424bffd
rmbodegalibre <rmbodegalibre@gmail.com>
2022-03-17 23:26:35 -0500

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,7 +13,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
-	
+
 	if (argc != 4)
 	{
 	printf("Error\n");
@@ -27,21 +27,21 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 	printf("Error\n");
-	
+
 	exit(99);
-	
+
 	}
 
 	if ((*op == '/' && num2 == 0) ||
 	(*op == '%' && num2 == 0))
-	
+
 	{
 	printf("Error\n");
-	
+
 	exit(100);
-	
+
 	}
-	
+
 	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
