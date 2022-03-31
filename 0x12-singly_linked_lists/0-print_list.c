74f73c9f818dbd0c6d3ef73b579cee03cc8f82af
rmbodegalibre <rmbodegalibre@gmail.com>
2022-03-30 22:02:07 -0500

    0. Print list 08

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 /**
  * print_list - Prints all the elements of a list_t list
  * @h: list_t
- * Return: number of nodes in h
+ * Return: number  of nodes in h
  */
 size_t print_list(const list_t *h)
 {
