3726251c437e80e43c472d6261426a2a7b55b639
rmbodegalibre <rmbodegalibre@gmail.com>
2022-04-02 23:30:53 -0500

    6. Pop 02

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,13 +9,15 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	listint_t *tempdata;
 
 	if (head == NULL)
 	return;
 	while (*head)
 	{
-	temp = (*head)->next;
-	free(*head);
-	*head = temp;
+	tempdata = (*head)->n;
+	temp = (*head);
+	(*head) = (*head)->next;
+	free(temp);
 	}
 }
