bf97edce38d524df1a525fee928a289b490ced67
rmbodegalibre <rmbodegalibre@gmail.com>
2022-04-02 21:30:01 -0500

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head != NULL)
+	if (head == NULL)
+	return;
+	while (*head)
 	{
-	temp = *head;
-	*head = *head->next;
+	temp = (*head)->next;
 	free(*head);
+	*head = temp;
 	}
 }
