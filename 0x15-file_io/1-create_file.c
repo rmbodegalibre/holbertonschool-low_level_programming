d9dc23f320a943f0e2c84ea284bd28a8883ebfce
rmbodegalibre <rmbodegalibre@gmail.com>
2022-04-06 23:53:50 -0500

    1. Under the snow 02

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT, S_IRUSR | O_TRUNC | S_IWUSR | O_WRONLY);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
 
 	if (fd < 0)
 		return (-1);
