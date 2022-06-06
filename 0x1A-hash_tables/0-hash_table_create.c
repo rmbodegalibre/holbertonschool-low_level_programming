871218541d083cb831605aec50ca8736d0b7ae37
rmbodegalibre <rmbodegalibre@gmail.com>
2022-06-06 00:52:35 -0500

    0x1A. C - Hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Function used to create a hash table
+ * @size: This is the size of array
+ *
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
