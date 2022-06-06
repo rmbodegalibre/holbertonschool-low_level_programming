0cbb26ca1b5c97e11f9a6d343055850784d8ccb6
rmbodegalibre <rmbodegalibre@gmail.com>
2022-06-06 00:57:54 -0500

    0x1A. C - Hash tables 02

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,7 +4,10 @@
 /**
  * hash_table_create - Function used to create a hash table
  * @size: This is the size of array
- *
+ * Return: Hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
+{
+return (0)
+}
