b2cfc8e7a3d7d8f8bb1bbc0156b8ade71e281abe
rmbodegalibre <rmbodegalibre@gmail.com>
2022-06-06 16:02:31 -0500

    1-djb2.c

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tavles.h"
+
+/**
+ * hash_djb2 - function to implementing the djb2 algorithm
+ * @str: Chain of chars received to generate hash value
+ *
+ * Return: Hash value
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
